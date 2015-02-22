7413bc98e52b853a7395337ef9d1fb111550f4c5
IFF999 <IFF999@users.noreply.github.com>
2015-02-22 13:07:54 -0500

    Initial Commit cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Stores the matrix as a global variable (cached)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	cachedInversedMatrix <<- x
 }
 
 
-## Write a short comment describing this function
+## Returns an uncached matrix for each new matrix used in the function call
+## otherwise returns a cached matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## Return a matrix that is the inverse of 'x'
+
+	if (exists("cachedInversedMatrix")) {
+		## we may have already solved the inverse but 
+		## is it for the same original matrix? - check it
+
+		inverseOfCached <- solve(cachedInversedMatrix)
+		if (isTRUE(all.equal(inverseOfCached,x)) )
+			s <- cachedInversedMatrix ## from cache
+		else {
+			s <- solve(x)
+			makeCacheMatrix(s)  ## store in cache 
+			}
+	} else { ## first call to function
+		s <- solve(x)
+		makeCacheMatrix(s)   ## store in cache
+				
+		}
+	return(s)
+
 }
